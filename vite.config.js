670d0ccfd38ace656c3567f187cdcf7937079de0
Solangeilinga <solangeilinga@gmail.com>
2025-12-04 10:29:22 +0100

    Deploy portfolio to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/' ,
-});
+  base: './', // IMPORTANT pour Netlify
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000
+  }
+})
